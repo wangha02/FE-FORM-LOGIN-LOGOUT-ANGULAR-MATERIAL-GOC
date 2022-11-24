980ea767f61a774f78b9880e227ca2a6287de072
Quang Hà <btkkentxx2002@gmail.com>
2022-11-24 09:14:18 +0700

    hp

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   API_LOCAL: 'http://localhost:8080/',
-  API_SEVER: 'https://jav06-test.herokuapp.com/',
+  API_SEVER: 'https://smileess.herokuapp.com/',
   production: true,
   firebaseConfig: {
     apiKey: "AIzaSyC8Y6q7CJXUKOQFHlGXZRo-ILM69u0tJrk",
